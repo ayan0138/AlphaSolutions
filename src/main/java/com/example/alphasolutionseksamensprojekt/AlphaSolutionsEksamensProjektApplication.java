f758acb298204a98670140b9e18cf881c15eb086
ayan0138 <ayah0002@stud.kea.dk>
2025-04-23 15:08:58 +0200

    test

---
+++ src/main/java/com/example/alphasolutionseksamensprojekt/AlphaSolutionsEksamensProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphasolutionseksamensprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaSolutionsEksamensProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaSolutionsEksamensProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alphasolutionseksamensprojekt/AlphaSolutionsEksamensProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alphasolutionseksamensprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaSolutionsEksamensProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
